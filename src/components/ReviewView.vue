962c7df214098676d116fbcfe2d7af786bc05db6
Zubairrafi <zubair@appscode.com>
2024-04-08 12:55:48 +0600

    Dipto vai rocks
    Signed-off-by: Zubairrafi <zubair@appscode.com>

--- src/components/ReviewView.vue
+++ src/components/ReviewView.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="description ml-5">
       <div class="comment">
-        <p>
+        <p class="review">
           I needed a year to actually adjust. I didn’t want to throw myself right into the course in the first
           <br />
           instance. I felt, because it was a different country, I would need time to figure myself out before
@@ -15,7 +15,7 @@
         </p>
       </div>
       <div class="designation">
-        <p>Sumeya from Kenya</p>
+        <p class="reviwer-name">Sumeya from Kenya</p>
         <p>Studying International Foundation Year in Humanities, Law and Social Science</p>
         <p>Aiming to progress to LLB (Hons) Law</p>
       </div>
@@ -44,14 +44,21 @@
   left: -20px;
   font-size: 24px;
 }
+.reviwer-name {
+  font-weight: 700;
+}
+
+.review {
+  font-size: 20px;
+}
 
 .comment {
-  background-color: #f0f0f0; /* Add background color to the comment */
-  padding: 10px; /* Add padding to the comment */
+  /* Add padding to the comment */
   border-radius: 5px; /* Add border radius to the comment */
 }
 
 .designation {
   margin-top: 10px; /* Add margin between comment and designation */
+  font-size: 14px;
 }
 </style>
